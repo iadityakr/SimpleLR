9f6bda099da940b788b376508e2d5fceaa8c5459
Aditya Kumar <74067573+iadityakr@users.noreply.github.com>
2021-05-31 12:54:34 +0530

    Add files via upload

---
+++ Simple_Linear_Regression.py
@@ -0,0 +1,10 @@
+import pandas
+import numpy
+from sklearn.linear_model import LinearRegression
+
+ds = pandas.read_csv('Salary_Data.csv')
+x = ds['YearsExperience'].values.reshape(30, 1)
+y = ds['Salary']
+model = LinearRegression()
+model.fit(x, y)
+model.predict([[ 1.1 ]])
